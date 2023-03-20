3d9161033997d810b0581050a7c92448465572e7
Anastasiia <nasik@Anastasiias-MacBook-Pro.local>
2023-03-20 23:16:48 +0200

    fix of 15th hw

--- 15_fifteenth_hw/test_human.py
+++ 15_fifteenth_hw/test_human.py
@@ -5,34 +5,42 @@ import pytest
 
 
 @pytest.mark.smoke
-def test_change_gender(create_female_age_30):
+def test_change_gender_to_male(create_female_age_30):
     human = create_female_age_30
     new_gender = 'male'
     human.change_gender(new_gender)
     assert human.gender == new_gender, f'Gender was not changed to {new_gender}'
 
 
+@pytest.mark.smoke
+def test_change_gender_to_female(create_male_age_40):
+    human = create_male_age_40
+    new_gender = 'female'
+    human.change_gender(new_gender)
+    assert human.gender == new_gender, f'Gender was not changed to {new_gender}'
+
+
 @pytest.mark.regression
-@pytest.mark.xfail
 def test_not_change_incorrect_gender(create_female_age_30):
     human = create_female_age_30
     new_gender = 'ololo'
-    human.change_gender(new_gender)
-    assert human.gender == new_gender, f'Gender was changed to {new_gender}'
+    with pytest.raises(Exception):
+        human.change_gender(new_gender)
+    assert human.gender != new_gender, f'Gender was changed to {new_gender}'
 
 
 @pytest.mark.regression
-@pytest.mark.xfail
 def test_not_change_the_same_gender(create_female_age_30):
     human = create_female_age_30
     the_same_gender = 'female'
-    human.change_gender(the_same_gender)
-    assert not human.gender == the_same_gender, f'Gender was changed to {the_same_gender}'
+    with pytest.raises(Exception):
+        human.change_gender(the_same_gender)
+    assert human.gender == the_same_gender, f'Gender was changed to {the_same_gender}'
 
 
 # there's a bug in the code logic "__is_alive"
 @pytest.mark.regression
-@pytest.mark.skip
+@pytest.mark.xfail
 def test_not_change_gender_of_dead_human(create_human_with_params):
     human = create_human_with_params(name='Mia', age=100, gender='female')
     new_gender = 'male'
@@ -49,17 +57,16 @@ def test_change_age(create_human_with_params):
 
 
 @pytest.mark.regression
-@pytest.mark.xfail
 def test_dead_age(create_human_with_params):
     age = 100
     human = create_human_with_params('Mia', age, 'female')
     human.grow()
-    assert human.age == age + 1, 'Human is already dead'
+    assert human.age != age + 1, 'Human is already dead'
 
 
 # there's a bug in the code logic "grow"
 @pytest.mark.regression
-@pytest.mark.skip
+@pytest.mark.xfail
 def test_not_born_human(create_human_with_params):
     age = -2
     human = create_human_with_params('Mia', age, 'female')
