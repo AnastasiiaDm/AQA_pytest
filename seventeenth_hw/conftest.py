28a9c4c8c2e321e08ed3923509ff554fcad755e7
Anastasiia <nasik@Anastasiias-MacBook-Pro.local>
2023-03-30 21:37:59 +0300

    test

--- seventeenth_hw/conftest.py
+++ seventeenth_hw/conftest.py
@@ -5,9 +5,12 @@ from seventeenth_hw.utilities.browser_factory import browser_factory
 from seventeenth_hw.page_objects.main_page_pack.main_page import MainPage
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_id', action='store', default=1, help='Set browser id')
+
 @pytest.fixture()
-def create_browser():
-    browser = browser_factory(get_browser_id())
+def create_browser(pytestconfig):
+    browser = browser_factory(int(pytestconfig.getoption('--browser_id')))
     browser.maximize_window()
     browser.get(get_application_url())
     yield browser
--- seventeenth_hw/utilities/browser_factory.py
+++ seventeenth_hw/utilities/browser_factory.py
@@ -1,8 +1,8 @@
 from selenium.webdriver import Chrome, Firefox
-from seventeenth_hw.utilities.config_reader import get_application_url
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.chrome.service import Service
+from argparse import ArgumentParser
 
 __CHROME = 1
 __FIREFOX = 2
@@ -15,3 +15,21 @@ def browser_factory(browser_id: int):
         return Firefox(service=Service(GeckoDriverManager().install()))
     else:
         return Chrome(service=Service(ChromeDriverManager().install()))
+
+#
+# class Chrome:
+#     def __init__(self):
+#         Chrome(service=Service(ChromeDriverManager().install()))
+#
+# class Firefox:
+#     def __init__(self):
+#         Firefox(service=Service(GeckoDriverManager().install()))
+
+# parser = ArgumentParser(description='Parser')
+# parser.add_argument('--browser', help='Browser name', default='chrome')
+# args = parser.parse_args()
+#
+# if args.browser.lower() == 'chrome':
+#     Chrome()
+# elif args.browser.lower() == 'firefox':
+#     Firefox()
